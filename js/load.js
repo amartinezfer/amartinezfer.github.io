ef3e34ce16fc6c82f4a57a5c97c027b2c4bb5d6c
Martínez Fernández <amafernandez@minsait.com>
2019-02-20 12:20:13 +0100

    manifest.json

--- js/load.js
+++ js/load.js
@@ -520,7 +520,19 @@ require(['app','jquery','bootstrap',"cookie"], function (App,jQuery) {
 
 // Execute App
 	$(document).ready(function(){
+		if ('serviceWorker' in navigator) {
+			window.addEventListener('load', function() {
+			  navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+				// Registration was successful
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			  }, function(err) {
+				// registration failed :(
+				console.log('ServiceWorker registration failed: ', err);
+			  });
+			});
 
+		  }
+		  
 		var userLang = navigator.language || navigator.userLanguage; 
 		
 		$('html').attr('lang',userLang.split('-')[0]);
